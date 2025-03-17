65c57f7ebc9917cfa5eac723c59c551990b2916e
uuuurrr <tacuio@naver.com>
2025-03-17 21:04:45 +0900

    2주차 실습 과제

--- inputTest.py
+++ inputTest.py
@@ -1,10 +1,22 @@
-num1=input("숫자1==> ")
+num1 = input("숫자1 ==> ")
 num1 = int(num1)
-print(num1)
-
-num2=input("숫자2==> ")
+num2 = input("숫자2 ==> ")
 num2 = int(num2)
-print(num2)
 
-numSum = num1+num2
-print(numSum)
+더하기 = num1 + num2
+print(더하기)
+
+빼기 = num1 - num2
+print(빼기)
+
+곱하기 = num1 * num2
+print(곱하기)
+
+나누기 = num1 / num2
+print(나누기)
+
+Modulus = num1%num2
+print(Modulus)
+
+거듭제곱 = num1**num2
+print(거듭제곱)
---
+++ inputTest2.py
@@ -0,0 +1,10 @@
+num1=input("숫자1==> ")
+num1 = int(num1)
+print(num1)
+
+num2=input("숫자2==> ")
+num2 = int(num2)
+print(num2)
+
+numSum = num1+num2
+print(numSum)
